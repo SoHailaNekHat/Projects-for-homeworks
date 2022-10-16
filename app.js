74600fd718a12f520c3bbcd42072aa1504d04258
SoHailaNekHat <sohaila.wb@gmail.com>
2022-10-16 20:08:27 +0430

    second challenge is done

--- app.js
+++ app.js
@@ -14,4 +14,11 @@ var john_hieght=1.75;
 var bmi_mark=mark_mass/(mark_hieght*mark_hieght);
 var bmi_john=john_mass/(john_hieght*john_hieght);
 var mark_is_higher=bmi_mark>bmi_john;
-console.log('is mark\'s BMI higher than john = '+mark_is_higher);
+console.log('Is mark\'s BMI higher than john = '+mark_is_higher);
+//challenge 2
+var team_1=(78+88+55+33+56+43);
+var team_2=(34+59+65+12+67+33);
+var team_1_avg=team_1/6;
+var team_2_avg=team_2/6;
+var compare=team_1_avg>team_2_avg
+console.  log('Is first team is the owner? \n '+compare);
