bb917481bfe47dc25bf3aadd904b9c45682992b5
SoHailaNekHat <sohaila.wb@gmail.com>
2022-10-16 20:17:22 +0430

    second challenge 4 part is done

--- app.js
+++ app.js
@@ -16,9 +16,16 @@ var bmi_john=john_mass/(john_hieght*john_hieght);
 var mark_is_higher=bmi_mark>bmi_john;
 console.log('Is mark\'s BMI higher than john = '+mark_is_higher);
 //challenge 2
-var team_1=(78+38+55+23+26+43);
-var team_2=(34+59+65+12+67+33);
-var team_1_avg=team_1/6;
-var team_2_avg=team_2/6;
-var compare=team_1_avg>team_2_avg
-console.  log('Is first team is the owner? \n '+compare);
+var team_1=(78+38+55);
+var team_2=(34+59+65);
+var mary_team=(97+134+105);
+var team_1_avg=team_1/3;
+var team_2_avg=team_2/3;
+var team_mary_avg=mary_team/3;
+
+var compare;
+if((team_mary_avg>team_2_avg) && (team_mary_avg)){
+  compare=true;
+}
+else{ compare=false;}
+console.  log('Is mary\'s team is the owner? \n '+compare);
