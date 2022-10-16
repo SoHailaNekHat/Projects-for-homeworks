3ac219e967064700be07279f76cb5a13aff756e2
SoHailaNekHat <sohaila.wb@gmail.com>
2022-10-16 19:59:51 +0430

    first challenge of the second section is done

---
+++ app.js
@@ -0,0 +1,17 @@
+//log to console
+// console.log('hello world');
+// console.log(123);
+// var greeting='Hello';
+// console.  log(greeting);
+// console.  log([1,2,3,4]);
+// var name='SoHaila';
+// console.  log(`${name}hello`);
+//Challenge 1
+var mark_mass=75;
+var mark_hieght=1.80;
+var john_mass=80;
+var john_hieght=1.75;
+var bmi_mark=mark_mass/(mark_hieght*mark_hieght);
+var bmi_john=john_mass/(john_hieght*john_hieght);
+var mark_is_higher=bmi_mark>bmi_john;
+console.log('is mark\'s BMI higher than john = '+mark_is_higher);
